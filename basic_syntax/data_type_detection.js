e66441d8fc30c20cc0c363e9fe73752f5274925c
Adityasputra <aditsputra.cv@gmail.com>
2025-05-04 16:36:54 +0700

    add a case study data type detection for basic syntax

---
+++ basic_syntax/data_type_detection.js
@@ -0,0 +1,15 @@
+let dataType = "Hello, World!";
+
+if (typeof dataType === "string") {
+  console.log("dataType is a string");
+} else if (typeof dataType === "number") {
+  console.log("dataType is a number");
+} else if (typeof dataType === "boolean") {
+  console.log("dataType is a boolean");
+} else if (typeof dataType === "object") {
+  console.log("dataType is an object");
+} else if (typeof dataType === "function") {
+  console.log("dataType is a function");
+} else {
+  console.log("Unknown data type");
+}
