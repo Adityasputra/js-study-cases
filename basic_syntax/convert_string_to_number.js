345f0e4907ce3a78f740a6ca36e972fbcee351be
Adityasputra <aditsputra.cv@gmail.com>
2025-05-05 00:17:38 +0700

    add a case study convert string to number for basic syntax

---
+++ basic_syntax/convert_string_to_number.js
@@ -0,0 +1,7 @@
+let string = "123";
+let number = Number(string);
+
+console.log("String: " + string);
+console.log("Number: " + number);
+console.log("Type of string: " + typeof string);
+console.log("Type of number: " + typeof number);
