d25e45f14734b3874308e0ebbce5bd289676e019
Adityasputra <aditsputra.cv@gmail.com>
2025-05-04 16:25:48 +0700

    add a case study full name format for basic syntax

---
+++ basic_syntax/full_name_format.js
@@ -0,0 +1,8 @@
+let firstName = "Minato";
+let lastName = "Aqua";
+
+const fullName = firstName + " " + lastName;
+console.log("Full Name (using +): " + fullName);
+
+const fullNameWithLiteral = `${firstName} ${lastName}`;
+console.log("Full Name (using template literal): " + fullNameWithLiteral);
