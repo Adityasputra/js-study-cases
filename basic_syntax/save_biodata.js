bc130ddd252075dfc5b06941ec803e87057391ce
Adityasputra <aditsputra.cv@gmail.com>
2025-05-05 00:28:27 +0700

    add a case study save biodata for basic syntax

---
+++ basic_syntax/save_biodata.js
@@ -0,0 +1,12 @@
+let name = "Noar"
+let age = "18"
+let tall = 170
+let weight = 60
+let isStudent = true
+
+console.log("Name: " + name)
+console.log("Age: " + age)
+console.log("Tall: " + tall)
+console.log("Weight: " + weight)
+console.log("Is Student: " + isStudent)
+console.log(`Description: ${name} is ${age} years old, ${tall} cm tall, weighs ${weight} kg, and is a student: ${isStudent}.`)
