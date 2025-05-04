853b2f60ef8894a7ce01aa26fd4c31ba385cabab
Adityasputra <aditsputra.cv@gmail.com>
2025-05-05 00:10:09 +0700

    add a case study simple calculator for basic syntax

---
+++ basic_syntax/simple_calculator.js
@@ -0,0 +1,16 @@
+let a = 12;
+let b = 8;
+
+const addition = a + b;
+const subtraction = a - b;
+const multiplication = a * b;
+const division = a / b;
+const modulus = a % b;
+const exponentiation = a ** b;
+
+console.log("Addition: " + addition);
+console.log("Subtraction: " + subtraction);
+console.log("Multiplication: " + multiplication);
+console.log("Division: " + division);
+console.log("Modulus: " + modulus);
+console.log("Exponentiation: " + exponentiation);
