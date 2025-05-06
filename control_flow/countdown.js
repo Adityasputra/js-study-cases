eaf4cd16e9c8e67d948c20564f2e529fd0d9bf06
Adityasputra <aditsputra.cv@gmail.com>
2025-05-06 21:51:59 +0700

    add a case study countdown for control flow

---
+++ control_flow/countdown.js
@@ -0,0 +1,19 @@
+// for method
+for (let i = 10; i >= 1; i--) {
+  console.log("i:", i);
+}
+
+// while method
+let j = 10;
+
+while (j >= 1) {
+  console.log("j:", j);
+  j--;
+}
+
+// do...while method
+let k = 10;
+do {
+  console.log("k:", k);
+  k--;
+} while (k >= 1);
