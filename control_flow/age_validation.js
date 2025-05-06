62eaba948eff91bc17e06c4070c56ec8e5ae2375
Adityasputra <aditsputra.cv@gmail.com>
2025-05-06 21:25:40 +0700

    add a case study age validation to enter the cinema for control flow

---
+++ control_flow/age_validation.js
@@ -0,0 +1,9 @@
+let age = 15;
+
+if (age < 13) {
+  console.log("not allowed to enter the cinema");
+} else if (age <= 17) {
+  console.log("must be accompanied by an adult");
+} else {
+  console.log("allowed to enter the cinema");
+}
